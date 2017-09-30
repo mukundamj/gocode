ac5acb7ddca54c4b8780449441b7623dee33398c
Mukunda <mukunda.mj@gmail.com>
2017-09-30 14:13:57 -0700

    dp program added

---
+++ algos/dp/uniqueBst.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*The logic implemented here is as per the leet code discussion using dynamic programming approach*/
+/*
+  The formula to find unique BSTs is as below
+  G(n) = G(0) * G(n-1) + G(1) * G(n-2) + … + G(n-1) * G(0)
+*/
+
+class Solution {
+  public:
+    int numTrees(int n) {
+      storedUniqueBst.resize(n, 0);
+      storedUniqueBst[0] = storedUniqueBst[1] = 1;
+      int sumUnique = 0;
+      for (int i = 1; i <= n; i++) {
+        sumUnique += _uniqueBst(i - 1)*_uniqueBst(n - i); 
+      }  
+      return sumUnique;
+    }
+  
+  private:
+    vector<int> storedUniqueBst;
+    int _uniqueBst(int n) {
+      if (storedUniqueBst[n] != 0) {
+        return storedUniqueBst[n];
+      }
+      for (int i = 1; i <= n; i++) {
+        storedUniqueBst[n] += _uniqueBst(i - 1)*_uniqueBst(n - i); 
+      }  
+      return storedUniqueBst[n];
+    }
+};
+
+int main(int argc, const char * argv[]) {
+  Solution S;
+  cout << "Running the c++ code " << argv[0] << endl;
+  cout << S.numTrees(atoi(argv[1])) << endl;
+  return 0;
+}
+
