4d31cc9f854fb60b64272d8dd7ca9161d173f6f3
Mukunda <mukunda.mj@gmail.com>
2017-10-26 20:51:57 -0700

    File added

---
+++ algos/hashTable/lengthOfLongestSubstring.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+  public:
+    int lengthOfLongestSubstring(string );
+};
+
+int Solution::lengthOfLongestSubstring(string s) {
+  if (s.size() == 0) return 0;
+  unordered_map<char, bool> sMap;
+  int max = 1, begin = 0, end = 1;
+  sMap[s[begin]] = true;
+  while (end < s.size()) {
+    if (sMap.find(s[end]) == sMap.end()) {
+      sMap[s[end++]] = true;
+    }  
+    else {
+      max = max >= (end - begin) ? max : (end - begin);
+      while (s[begin] != s[end]) {
+        sMap.erase(s[begin]);
+        begin++; 
+      }
+      begin++;
+      end++;
+    }
+  }
+  max = max >= (end - begin) ? max : (end - begin);
+  return max;
+}
+
+
+int main(int argc, const char* argv[]) {
+  Solution S;
+  string s = "abcbb";
+  cout << "Length of longest substring is " << S.lengthOfLongestSubstring(s) << endl;
+  return 0;
+}
+
