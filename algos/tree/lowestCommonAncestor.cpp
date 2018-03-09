ddb49ef928227cad937c18fad752262223dd36c0
mj <mukundamj@users.noreply.github.com>
2018-03-08 22:55:32 -0800

    Added comment

--- algos/tree/lowestCommonAncestor.cpp
+++ algos/tree/lowestCommonAncestor.cpp
@@ -15,6 +15,11 @@ class Solution {
     TreeNode* lowestCommonAncestor(TreeNode*, TreeNode*, TreeNode*); 
 };
 
+//Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+//According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes 
+//v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a 
+//descendant of itself).” 
+
 TreeNode* Solution::lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
   if (!root || root == p || root == q) return root;
   TreeNode* left = lowestCommonAncestor(root->left, p, q);
