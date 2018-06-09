c63bca1ccedcfef08c6e25473ada418b82b5dde6
mj <mukundamj@users.noreply.github.com>
2018-06-09 13:01:16 -0700

    Comment added

--- algos/bfs/findMinHeightTrees.cpp
+++ algos/bfs/findMinHeightTrees.cpp
@@ -14,6 +14,13 @@ struct Node {
   bool isLeaf() const { return neighbor.size() == 1; }
 };
 
+/*
+  For a undirected graph with tree characteristics, we can choose any node as 
+  the root. The result graph is then a rooted tree. Among all possible rooted
+  trees, those with minimum height are called minimum height trees (MHTs). 
+  Given such a graph, write a function to find all the MHTs and return a list 
+  of their root labels.
+*/
 vector<int> Solution::findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
   vector<int> buf1;
   vector<int> buf2;
